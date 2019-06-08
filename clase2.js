92d93b6e56af1d3acf393eb3a6d148f96037f19a
Luis-Dokkaebi <14470208@itcampeche.edu.mx>
2019-06-08 00:36:18 -0500

    Variables Strings

---
+++ clase2.js
@@ -0,0 +1,15 @@
+var nombre = 'Luis';
+var apellido = 'Aguilar';
+
+
+var nombreEnMayusculas = nombre.toUpperCase();
+var apellidoEnMinisculas = apellido.toLowerCase();
+
+
+var primeraLetraDelNombre = nombre.charAt(0);
+
+var cantidadDeLetrasDelNombre = nombre.length
+
+var nombreCompleto = `${nombre.toLowerCase()} ${apellido.toUpperCase()}`
+
+var str = nombre.substr(1,2)
