5c8893956779244dd4d02e660a794318124cc58d
Luis-Dokkaebi <14470208@itcampeche.edu.mx>
2019-06-08 00:17:34 -0500

    Añadiendo variables

--- clase1.js
+++ clase1.js
@@ -1,1 +1,6 @@
-console.log('Hola mundo JS')
+var nombre = 'Luis';
+var apellido = 'Aguilar';
+var edad = 28
+
+console.log('Hola ' + nombre + ' ' + apellido);
+console.log('Tengo '+ edad + ' años');
