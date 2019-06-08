1cbe2e53112680182f27e547fc72eb11a27b13a7
Luis-Dokkaebi <14470208@itcampeche.edu.mx>
2019-06-08 06:29:39 -0500

    Variables con numeros

---
+++ clase3.js
@@ -0,0 +1,28 @@
+var edad = 22
+
+//edad = edad + 1
+edad += 1
+
+var peso = 77
+
+// peso = peso - 2
+peso -= 2
+
+var sandwich = 1
+
+peso += sandwich
+
+var jugarAlFutbol = 3
+
+peso -= jugarAlFutbol
+
+var precioDeVino = 200.3
+
+var total = Math.round(precioDeVino * 100 * 3) / 100
+var totalstr = total.toFixed(3)
+var total2 = parseFloat(totalstr)
+
+var pizza = 8
+var personas = 2
+
+var cantidadDePorcionasPorPersona = pizza / personas
