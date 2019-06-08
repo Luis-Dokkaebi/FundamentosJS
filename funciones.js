8c337508da470d174caa84e89859b18f57ec3d7a
Luis-Dokkaebi <14470208@itcampeche.edu.mx>
2019-06-08 06:41:49 -0500

    Agregando funciones

---
+++ funciones.js
@@ -0,0 +1,12 @@
+var nombre = 'Luis', edad = 22
+
+// aceptar parametros
+function imprimirEdad(n, e){
+  console.log (`${n} tiene ${e} años`)
+}
+
+// hay que escribir los parametrosen la funcion
+imprimirEdad(nombre, edad)
+imprimirEdad('Vicky ', 24)
+imprimirEdad('Carlos', 25)
+imprimirEdad('Rambo', 23)
